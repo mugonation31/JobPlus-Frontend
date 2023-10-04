81355d0a42cd6e9d5c001b645291deefb21e1c4c
Simba Mugomezi <simbadip@hotmail.co.uk>
2023-10-04 22:53:55 +0100

    created useCookie custom hook

---
+++ src/hooks/useCookie.js
@@ -0,0 +1,19 @@
+export const useCookie = () => {
+  const saveAuthCookie = (key) => {};
+
+  const deleteAuthCookie = (key) => {};
+
+  const getAuthCookie = (key) => {};
+
+  const isAuthCookieExpired = (key) => {};
+
+  const hasValidAuthCookie = (key) => {};
+
+  return {
+    saveAuthCookie,
+    deleteAuthCookie,
+    getAuthCookie,
+    isAuthCookieExpired,
+    hasValidAuthCookie,
+  };
+};
