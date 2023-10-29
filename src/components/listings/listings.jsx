620a8d1359fb58abcf914c8edb625152ceb8d125
Simba Mugomezi <simbadip@hotmail.co.uk>
2023-10-29 23:16:36 +0000

    connected listings to backend and automated pagination

--- src/components/listings/listings.jsx
+++ src/components/listings/listings.jsx
@@ -1,22 +1,47 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./listings.scss";
-
 import Paginate from "../paginate/paginate";
-
 import { StarSaved, Money, Location, Timer } from "../images";
+import { useApi } from "../../hooks/useApi";
+
+const MAX_PER_PAGE = 3;
 
 export default function listings() {
+  const [jobs, setJobs] = useState([]);
+  const [meta, setMeta] = useState({});
+
+  const { get } = useApi();
+
+  const handleSuccess = (res) => {
+    const { entries, meta } = res.data;
+    setJobs(entries);
+    setMeta(meta);
+  };
+
+  const fetchJobs = async (page = 1) => {
+    await get("jobs", {
+      onSuccess: (res) => handleSuccess(res),
+      params: {
+        "populate[company]": true,
+        start: (page - 1) * MAX_PER_PAGE,
+        limit: MAX_PER_PAGE,
+      },
+    });
+  };
+
+  useEffect(() => {
+    fetchJobs();
+  }, []);
+  console.log(jobs);
   return (
     <section>
-      <a href="apply.html">
-        <div className="listing__card">
+      {jobs.map((job, index) => (
+        <div key={index} className="listing__card">
           <header className="listing__header">
-            <h1 className="listing__title">
-              Regulatory Affairs Senior Manager
-            </h1>
+            <h1 className="listing__title">{job.title}</h1>
             <img className="listing__saved" src={StarSaved} alt="" />
             <p className="listing__company">
-              Posted by <span>Koco Media</span>
+              Posted by <span>{job.company.name}</span>
             </p>
           </header>
 
@@ -36,15 +61,14 @@ export default function listings() {
           </ul>
 
           <p className="listing__detail">
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Explicabo,
-            asperiores ullam minus at <b>Read more...</b>
+            {job.description} <b>Read more...</b>
           </p>
 
           <a href="" className="listing__cta">
             Withdraw application
           </a>
         </div>
-      </a>
+      ))}
       <Paginate />
     </section>
   );
