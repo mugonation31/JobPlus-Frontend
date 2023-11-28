05b01169257f2ddf24c393aa1ba5622ae8d52a88
Simba Mugomezi <simbadip@hotmail.co.uk>
2023-11-28 22:28:28 +0000

    completed jobService

--- src/services/JobService.js
+++ src/services/JobService.js
@@ -2,4 +2,23 @@ import { useApi } from "../hooks/useApi";
 
 const jobService = () => {
   const { get } = useApi();
+  const MAX_PER_PAGE = 3;
+
+  const fetchJobs = async (page = 1, onSuccess) => {
+    await get("jobs", {
+      onSuccess: onSuccess,
+      params: {
+        "populate[company]": true,
+        "populate[job_types]": true,
+        start: (page - 1) * MAX_PER_PAGE,
+        limit: MAX_PER_PAGE,
+      },
+    });
+  };
+
+  return {
+    fetchJobs,
+  };
 };
+
+export default jobService;
